4f62de9a9ac8b99cc90d9824c59ccd180e1e27c3
Romain Siegfriedt <romain.siegfriedt@etu.univ-orleans.fr>
2017-06-14 14:13:12 +0200

    création du controleurAccueilSondeur et amelioration du VueAccueil sondeur

--- src/VueAccueilSondeur.java
+++ src/VueAccueilSondeur.java
@@ -8,57 +8,62 @@ public class VueAccueilSondeur extends JPanel{
 	EasySond sond;
 	Questionnaire questionnaire;
 	ArrayList<Sonde> listeSonde;
+  ControleurAccueilSondeur actionBoutons;
 
 	VueAccueilSondeur(EasySond sond){
 		super();
 		this.sond = sond;
+		this.actionBoutons= new ControleurAccueilSondeur(this);
 		this.questionnaire = this.sond.basededonnes.BDaccueilSondeur.GetQuestionnaire();
 		this.listeSonde = this.sond.basededonnes.BDaccueilSondeur.GetListeSonde(this.questionnaire.getIdentifiantPanel());
-		pageGenerator(false);
+		this.add(pageGenerator(false));
 		this.setVisible(true); //affiche le tout
 	}
 
 
-	void refresh(boolean e){
-		this.removeAll();
-		pageGenerator(e);
-		this.repaint();
-		this.validate();
-	}
+	/*void refresh(boolean e){
+		Container cont=this.sond.getContentPane();
+		cont.removeAll();
+		cont.add(pageGenerator(e));
+		cont.repaint();
+		cont.validate();
+	}*/
 
-	void pageGenerator(boolean e){
-		//Container Principal
-		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+	JPanel pageGenerator(boolean e){
+		JPanel p = new JPanel();
+		p.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 			//niveau 1, Annonce la page.
 		JPanel lv1= new JPanel();
 		lv1.setLayout(new FlowLayout(FlowLayout.LEFT));
 		lv1.add(nomPage());
-		this.add(lv1);
+		p.add(lv1);
 
 			//niveau 2, informe sur le sondage en question.
 		JPanel lv2= new JPanel();
 		lv2.setLayout(new FlowLayout());
 		lv2.add(nomSondage());
-		this.add(lv2);
+		p.add(lv2);
 
 			//niveau 3, informe sur le sondé en question.
 		JPanel lv3= new JPanel();
 		lv3.setLayout(new FlowLayout());
 		lv3.add(infoSonde());
-		this.add(lv3);
+		p.add(lv3);
 
 			//niveau 4, disposition des bouton.
 		JPanel lv4= new JPanel();
 		lv4.setLayout(new FlowLayout());
 		lv4.add(boutons(e));
-		this.add(lv4);
+		p.add(lv4);
 
 			//niveau 5, bouton help
 		JPanel lv5= new JPanel();
 		lv5.setLayout(new FlowLayout(FlowLayout.RIGHT));
 		lv5.add(help());
-		this.add(lv5);
+		p.add(lv5);
 
+
+		return p;
 	}
 
 	JPanel nomPage(){	//Annonce de la page
@@ -111,11 +116,13 @@ public class VueAccueilSondeur extends JPanel{
 		JButton b3= new JButton("Passer au sondé suivant ");
 		if(e==false){
 			JButton b1= new JButton("    Appeler le sondé    ");
+			b1.addActionListener(this.actionBoutons);
 			panelBoutons.add(b1);
 			b2.setEnabled(false);
 		}
 		else{
 			JButton b1= new JButton("Appel en cours (annuler)");
+			b1.addActionListener(this.actionBoutons);
 			panelBoutons.add(b1);
 			b3.setEnabled(false);
 		}
